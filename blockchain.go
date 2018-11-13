5c05ffda5638cc4df5ab9ad7de67535c85315316
Matrix <matrix@MatrixdeMacBook-Pro.local>
2018-11-13 14:45:24 +0800

    整理项目结构

--- blockchain.go
+++ blockchain.go
@@ -1,43 +1,7 @@
 package main
 
-import (
-	"time"
-	"crypto/sha256"
-	"strconv"
-	"fmt"
-	"bytes"
-)
-
-type Block struct {
-	Timestamp 		int64
-	Data			[]byte
-	PrevBlockHash	[]byte
-	Hash	[]byte
-}
-
 type Blockchain struct {
 	blocks []*Block
-}
-
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash,
-									b.Data,
-									timestamp}, 
-									[]byte{})
-	hash := sha256.Sum256(headers)
-	
-	b.Hash = hash[:]
-}
-
-func NewBlock(data string, PrevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(),
-					[]byte(data),
-					PrevBlockHash,
-					[]byte{}}
-	block.SetHash()
-
-	return block
 }
 
 func (bc *Blockchain) AddBlock(data string) {
@@ -46,23 +10,9 @@ func (bc *Blockchain) AddBlock(data string) {
 	bc.blocks = append(bc.blocks, newBlock)
 }
 
-func NewGenesisBlock() *Block {
-	return NewBlock("Genesis Block", []byte{})
-}
 
 func NewBlockchain() *Blockchain {
 	return &Blockchain{[]*Block{NewGenesisBlock()}}
 }
 
-func main() {
-	bc := NewBlockchain()
-	bc.AddBlock("Send 1 BTC to Ivan")
-	bc.AddBlock("Send 2 more BTC to Ivan")
 
-	for _, block := range bc.blocks {
-		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Println()
-	}
-}
