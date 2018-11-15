bb870ff6a599068d5f329fcb1d23da54d643d61d
Matrix <matrix@MatrixdeMacBook-Pro.local>
2018-11-15 18:20:25 +0800

    ADD: 迭代器完成

--- blockchain.go
+++ blockchain.go
@@ -8,10 +8,17 @@ import (
 
 const dbFile = "blockchain.db"
 const blocksBucket = "blocks"
+
 type Blockchain struct {
 	tip []byte
 	db *bolt.DB
 }
+
+type BlockchainIterator struct {
+	currentHash []byte
+	db 			*bolt.DB
+}
+
 
 func (bc *Blockchain) AddBlock(data string) {
 	var lastHash []byte
@@ -96,6 +103,31 @@ func NewBlockchain() *Blockchain {
 	return &bc
 }
 
+func (bc *Blockchain) Iterator() *BlockchainIterator {
+	bci := &BlockchainIterator{bc.tip, bc.db}
 
+	return bci
+}
+
+
+func (i *BlockchainIterator) Next() *Block {
+	var block *Block
+
+	err := i.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		encodedBlock := b.Get(i.currentHash)
+		block = DeserializeBlock(encodedBlock)
+
+		return nil
+	})
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	i.currentHash = block.PrevBlockHash
+
+	return block
+}
 
 
